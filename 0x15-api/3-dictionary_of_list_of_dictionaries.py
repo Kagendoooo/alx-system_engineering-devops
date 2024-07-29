2db991cdf64ceccab3b35837d30075a723d74d50
Kagendoooo <kagendonikki16@gmail.com>
2024-07-30 02:22:43 +0300

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format.
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            user.get("id"): [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username")
+            } for task in requests.get(
+                url + "todos",
+                params={"userId": user.get("id")}
+            ).json()]
+            for user in users
+        }, jsonfile)
