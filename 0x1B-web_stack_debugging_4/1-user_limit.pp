2712de765995128c46e39d4a2013e48868cd3408
Kagendoooo <kagendonikki16@gmail.com>
2024-08-23 00:47:21 +0300

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# allow holberton super user to login & open files
+
+exec { 'increase hard file limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase soft file limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
